fce95d59c2c396544eba21f620f382edc121c708
Caleb <ande4905@gmail.com>
2020-03-21 13:35:20 -0500

    making flowchart, enhancments

--- public_html/footer.php
+++ public_html/footer.php
@@ -1,8 +1,13 @@
 <div class="navbar">
 <footer>
     <p>&copy; Chasing the Arctic.<br />
-        Please see our other information
+        Please see our other information.
     <a href="#">Other info</a>.</p>
+    <p>We used a few open source projects for this website.<br />
+    <a href='https://github.com/RobThree/TwoFactorAuth'>RobThree2FA</a><br />
+    <a href='https://greensock.com/'>GreenSock Animation</a><br />
+    <a href='https://fonts.google.com/?selection.family=Montserrat'>Google Fonts</a><br />
+    <a href='https://html5boilerplate.com/'>HTML5 Boilerplate</a>
 </footer>
 </div>
 </body>
