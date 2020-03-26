78e3162a426277497a89679bd640520825557f65
Caleb <ande4905@gmail.com>
2020-03-26 14:27:28 -0500

    started input sanitization

--- public_html/add_member.php
+++ public_html/add_member.php
@@ -5,21 +5,36 @@
 <fieldset class="fieldset">
 <legend>Add Member Result</legend>
 <?php
-    if(!isset($_POST['username']) || !isset($_POST['pswd'])){
-        echo "<p>You have not entered all the required details.<br />
-            Please go back and try again.</p>";
-        exit;
+    //test and store all input
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
     }
-    // create short variable names
-    $username = $_POST['username'];
-    $_SESSION['username'] = $username;
-    $pswd = $_POST['pswd'];
-    $db = new mysqli('localhost', 'caleb_tree', '***REMOVED***', '***REMOVED***');
-    if(mysqli_connect_errno()){
-        echo "<p>Error: Could not connect to database.<br/>
-            Please try again later.</p>";
-        exit;
-    }    
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $fName = test_input($_POST['fname']);
+        // check if name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z ]*$/",$fName)) {
+            $fNameErr = "Only letters and white space allowed. ";
+        }
+        $lName = test_input($_POST['lname']);
+        // check if name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z ]*$/",$lName)) {
+            $lNameErr = "Only letters and white space allowed. ";
+        }
+        
+        // create short variable names
+        $username = $_POST['username'];
+        // $_SESSION['username'] = $username;
+        $pswd = $_POST['pswd'];
+        $db = new mysqli('localhost', 'caleb_tree', '***REMOVED***', '***REMOVED***');
+        if(mysqli_connect_errno()){
+            echo "<p>Error: Could not connect to database.<br/>
+                Please try again later.</p>";
+            exit;
+        }
+    }
     //hash password
     $hash = password_hash($pswd, PASSWORD_DEFAULT);
     
@@ -37,6 +52,11 @@
             exit;
     }
     $db->close();
+    echo '<p>This is your first name: '.$fName.'</p>';
+    echo '<p>This is your last name: '.$lName.'</p>';
+    echo '<p>This is your username: '.$username.'</p>';
+    echo '<p>This is your password: '.$pswd.'</p>';
+    echo '<p>This is your hashed password: '.$hash.'</p>';
 ?>
 <p>Try one of these great apps to setup multi-factor authentication: </p>
     <a href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2&hl=en"><img src="https://lh3.googleusercontent.com/HPc5gptPzRw3wFhJE1ZCnTqlvEvuVFBAsV9etfouOhdRbkp-zNtYTzKUmUVPERSZ_lAL=s180" alt="Google Authenticator"></a>
@@ -44,8 +64,8 @@
     <a href="https://play.google.com/store/apps/details?id=com.azure.authenticator&hl=en"><img src="https://lh3.googleusercontent.com/MkSEE1fAQDUfQsqlhUEat-ZzJjkR2XnJEJpE-abcn7dPcIN8aYocgLoIDhujEDD5w-g=s180" alt="Authy 2-Factor Authentication"></a>
 <form action="setup_2fa.php" method="get" id="mainForm">
 <p>Would you like to setup Multi-Factor Authentication?</p>
-<span><input name="yes" type="submit" value="Yes" class="btn"/></span>
-<span><input name="no" type="submit" value="No" class="btn"/></span>
+<span><button name="yes" type="submit">Yes</button></span>
+<span><button name="no" type="submit">No</button></span>
 </form>
 </fieldset>
 <?php
