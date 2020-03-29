d06be6b4c20158a7ebc762c348db2d56887e8af0
tthao <tongthaithao@gmail.com>
2020-03-28 22:29:19 -0500

    Changed the layout of the main page. Changed the way that the header looked. Added Carousel. Added Test banner also.

--- public_html/header.php
+++ public_html/header.php
@@ -1,47 +1,39 @@
 <!DOCTYPE html>
-<html class="no-js" lang="en" xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta charset="utf-8" />
-    <meta name="description" content="environmental resources">
-    <meta name="author" content="???">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="icon" href="favicon.ico">
-
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/styles.css" />
-    <link rel="stylesheet" href="css/popover.css" />
-    
-    <meta name="theme-color" content="#fafafa">
-
-    <script src="js/script.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.2.4/gsap.min.js"></script>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    
-    <title>Antarctica</title>    
-</head>
-<body id="main">
-<img src="images/logo.png" class="logo">
-
-<div class="navbar">
-<script>gsap.from(".logo", {duration: 1.5, opacity: 0, scale: 0.3, ease: "back"});</script>
-    <nav id="shoppingNav">
-        <a href="home.php">Home</a>
-        <a href="new_member_form.php">Become a Member</a>        
-        <a href="sign_in_form.php">Sign In</a>   
-        <span id='navbtn' onclick="toggleNav()">&#9776; Nav Menu</span>           
-    </nav>
-    </div>
-    
-    <div id="websiteNav" class="sidenav">        
-        <a href="javascript:void(0)" id="navclosebtn" onclick="closeNav()">&times;</a>
-        <!-- <a href="#" class="navicon" id="expicon">Expeditions</a> -->
-        <a href="#" class="navicon" id="newsicon">News</a>
-        <a href="#" class="navicon" id="mbricon">Membership</a>        
-        <a href="web_store.php" class="navicon" id="storeicon">Store</a>
-        <a href="#" class="navicon" id="discourseicon">Discourse</a>
-        <a href="my_robThree_2FA.php" id="">2FA Demo</a>
-    </div> 
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta charset="utf-8">
+        <title>Chasing Antartica</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="page icon" href="favicon.ico" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    </head>
+    <body>
+        <section>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Missions</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Help</a>
+                    </li>
+                  </ul>
+                  <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                  </form>
+                </div>
+              </nav>
+        </section>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </body>
+</html>
--- public_html/home.php
+++ public_html/home.php
@@ -1,6 +1,26 @@
 <?php
 require('header.php');
 ?>
+<style>
+  .welcome {
+    padding: 20px;
+  }
+  .banner {
+    background-color: #80ff80;
+    height: 300px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+  }
+  .carouselHeader {
+    padding-top: 50px;
+    padding-bottom: 100px;
+  }
+  .carousel-container{
+  margin: auto;
+  width: 85%;
+  padding: 10px;
+}
+</style>
   <script src="js/vendor/modernizr-3.8.0.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
   <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.4.1.min.js"><\/script>')</script>
@@ -8,11 +28,40 @@ require('header.php');
   <script src="js/main.js"></script>
 <!-- page content -->
 <article class="fieldset">
-    <h1>Welcom to Chasing the Arctic NPO 501(c)</h1>
+    <h1 align="center" class="welcome">Welcome to Chasing Antartica NPO 501(c)</h1>
     <section>
-        <h2>This website provides up to date information on environmental issues.</h2>
+        <h2 align="center" class="welcome">This website provides up to date information on environmental issues.</h2>
     </slection>
 </article>
+<div>
+  <h2 class="banner" align="center">Banner Test</h2>
+</div>
+<div>
+  <h2 class="carouselHeader" align="center">A Place Further than the Universe</h2>
+</div>
+<div class="carousel-container">
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100" src="images/Iceberg.jpg" alt="Iceberg" width="500" height="600">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="images/Penguins.jpg" alt="Penguins">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="images/Ship.jpg" alt="Giant Ship">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
 <?php
 require('footer.php');
 ?>
