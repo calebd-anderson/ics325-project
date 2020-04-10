b48d42f581493f1301c30ee62da15cb2e5446596
Caleb <ande4905@gmail.com>
2020-04-10 15:18:26 -0500

    neatifying

--- public_html/sign_in_form.php
+++ public_html/sign_in_form.php
@@ -1,8 +1,4 @@
 <?php
-// ini_set('display_startup_errors',1); 
-// ini_set('display_errors',1);
-// error_reporting(-1);
-// session_start();
 include 'header.php';
 require '../SQLcreds.inc';
 
@@ -63,13 +59,11 @@ if ((!isset($_POST['username'])) || (!isset($_POST['pswd']))) {
     <p><label for="pswd">Password:</label>
       <span class="eye_container"> 
         <input type="password" name="pswd" id="pswd" size="20" class="required highlightable"/>
-        <a class="input_img" onclick="toggle_pswd()">
-        <span id="eye">
-          <svg class="bi bi-eye" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <a class="input_icon" onclick="toggle_pswd()">
+          <svg id="eye" class="bi bi-eye" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.134 13.134 0 001.66 2.043C4.12 11.332 5.88 12.5 8 12.5c2.12 0 3.879-1.168 5.168-2.457A13.134 13.134 0 0014.828 8a13.133 13.133 0 00-1.66-2.043C11.879 4.668 10.119 3.5 8 3.5c-2.12 0-3.879 1.168-5.168 2.457A13.133 13.133 0 001.172 8z" clip-rule="evenodd"/>
             <path fill-rule="evenodd" d="M8 5.5a2.5 2.5 0 100 5 2.5 2.5 0 000-5zM4.5 8a3.5 3.5 0 117 0 3.5 3.5 0 01-7 0z" clip-rule="evenodd"/>
           </svg>
-        </span>
         </a>
       </span>
       <p id="CapsLk">WARNING! Caps lock is ON.</p>
@@ -85,7 +79,7 @@ if ((!isset($_POST['username'])) || (!isset($_POST['pswd']))) {
 <?php
 
 //if sibmitted check password and username
-}else if(password_verify($pswd, $value)) {
+} else if(password_verify($pswd, $value)) {
   // visitor's name and password combination are correct
   //extract and decrypt secret
   $sql = "SELECT AES_DECRYPT(secret, UNHEX('$key')) FROM member_creds WHERE username = '$username' LIMIT 1";
@@ -105,11 +99,7 @@ if ((!isset($_POST['username'])) || (!isset($_POST['pswd']))) {
       </form>
 <?php
     $_SESSION['secret'] = $secret;
-    // $_SESSION['username'] = $username;
   }
-  // $conn->close();
-  // header("Location:check_2fa.php");
-  // $_SESSION['valid_user'] = $username;
   if($secret == null){
     $_SESSION['valid_user'] = $username;
     echo '<section><h2>You\'re Signed In</h2>
@@ -122,18 +112,18 @@ if ((!isset($_POST['username'])) || (!isset($_POST['pswd']))) {
   <a target="_blank" href="https://play.google.com/store/apps/details?id=com.azure.authenticator&hl=en"><img src="https://lh3.googleusercontent.com/T7_4mv5aTo4XdLWz_vAQ2Hg8E-uMf_XUZMtdHdnf5ifZEP413DhOEDo2YEMSSL1Jl4k=s180" alt="Microsoft Authenticator" width="100" height="auto"></a>
   <form action="setup_2fa.php" method="get" id="mainForm">
   <p>Would you like to setup multi-factor authentication now?</p>
-  <span><button name="yes" type="submit">Yes</button></span>
-  <span><button name="no" type="submit">No</button></span>
+  <span><button class="btn btn-primary" name="yes" type="submit">Yes</button></span>
+  <span><button class="btn btn-secondary" name="no" type="submit">No</button></span>
   </section>
   </form>
 <?php    
-  }
-} else{
-    echo'<fieldset class="fieldset"><h1>Go Away!</h1>';
-    echo '<p>You are not authorized to use this resource.</p></fieldset>';
-    $conn->close();
-  }
-include 'footer.php';
+    }
+  } else{
+      echo'<fieldset class="fieldset"><h1>Go Away!</h1>';
+      echo '<p>You are not authorized to use this resource.</p></fieldset>';
+      $conn->close();
+    }
+  include 'footer.php';
 ?>
 
 <script>  
