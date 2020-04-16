80a67957104b1fb9b395c63c2cce332dc4450079
Caleb <ande4905@gmail.com>
2020-04-15 21:33:36 -0500

    resized carosel images

--- public_html/new_member_form.php
+++ public_html/new_member_form.php
@@ -25,31 +25,6 @@
 </script>
 <fieldset class="fieldset"><legend style="font-family: Raleway;">Chasing Arctic - Register</legend>
     <form action="add_member.php" method="post" id="stepForm">
-<!-- Login -->
-    <div class="tab"><h5>Login Info:</h5>
-            <p>
-                <input class="form-control" type="text" id="username" placeholder="Username..."
-                    name="username" maxlength="25"  onBlur="checkAvailability()"
-                    oninput="this.className = 'form-control'"/>
-                <span class="fa fa-cog fa-spin" id="loaderIcon" style="font-size:24px; display:none; color:#0084f0;"></span>
-                <span id="user-availability-status"></span>
-            </p>
-            <p>
-                <!-- <input id="password" class="form-control" type="password" placeholder="Enter the password"> -->
-                <input id="pswd" class="form-control" tabindex="0" placeholder="Password..." type="password"
-                    name="pswd" maxlength="100" size="20" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                    data-animation="true" data-toggle="popover" data-trigger="focus" data-placement="left" data-content="Try to pick something that’s not easy to guess." 
-                    oninput="this.className = 'form-control'"/>
-                <?php //echo "$pswdErr";?>
-            </p>
-            <p id="CapsLk">WARNING! Caps lock is ON.</p>
-            <div id="message" class="js-fade">
-                <p id="letter" class="invalid">A <b>lowercase</b> letter</p>
-                <p id="capital" class="invalid">A <b>capital (uppercase)</b> letter</p>
-                <p id="number" class="invalid">A <b>number</b></p>
-                <p id="length" class="invalid">Minimum <b>8 characters</b></p>
-            </div>
-        </div>
 <!-- Name -->
         <div class="tab"><h5>Name:</h5>
             <p><input class="form-control" type="text" name="fname" placeholder="First name..." oninput="this.className = 'form-control'"></p>
@@ -61,6 +36,31 @@
             <p><input class="form-control" type="text" placeholder="Address..." name="addr" oninput="this.className = 'form-control'"></p>
             <p><input class="phone_us form-control" type="tel" placeholder="Phone..." name="us_phone" pattern="^[0-9-+\s()]*$" oninput="this.className = 'form-control'"></p>
         </div>
+<!-- Login -->
+        <div class="tab"><h5>Login Info:</h5>
+                <p>
+                    <input class="form-control" type="text" id="username" placeholder="Username..."
+                        name="username" maxlength="25"  onBlur="checkAvailability()"
+                        oninput="this.className = 'form-control'"/>
+                    <span class="fa fa-cog fa-spin" id="loaderIcon" style="font-size:24px; display:none; color:#0084f0;"></span>
+                    <span id="user-availability-status"></span>
+                </p>
+                <p>
+                    <!-- <input id="password" class="form-control" type="password" placeholder="Enter the password"> -->
+                    <input id="pswd" class="form-control" tabindex="0" placeholder="Password..." type="password"
+                        name="pswd" maxlength="100" size="20" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                        data-animation="true" data-toggle="popover" data-trigger="focus" data-placement="left" data-content="Try to pick something that’s not easy to guess." 
+                        oninput="this.className = 'form-control'"/>
+                    <?php //echo "$pswdErr";?>
+                </p>
+                <p id="CapsLk">WARNING! Caps lock is ON.</p>
+                <div id="message" class="js-fade">
+                    <p id="letter" class="invalid">A <b>lowercase</b> letter</p>
+                    <p id="capital" class="invalid">A <b>capital (uppercase)</b> letter</p>
+                    <p id="number" class="invalid">A <b>number</b></p>
+                    <p id="length" class="invalid">Minimum <b>8 characters</b></p>
+                </div>
+            </div>
 <!-- Buttons -->
         <div style="overflow:auto;">
             <div style="float:right;">
