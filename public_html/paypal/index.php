4f1dd0a0e94719ab057bb80f5a7bcb63897f4f8c
Caleb <ande4905@gmail.com>
2020-05-01 15:09:22 -0500

    paypal integration draft 1

--- public_html/paypal/index.php
+++ public_html/paypal/index.php
@@ -1,23 +1,56 @@
-<?php
-    include('header.php');
-?>
+<html>
+<title>Paypal Payment Gateway Integration in PHP</title>
+<head>
+<style>
+body {
+    font-family: Arial;
+    line-height: 30px;
+    color: #333;
+}
 
-<h1>I Will Manage Your Membership</h1>
-<!--
-    - make a form with recuring payment date select 
-    - amount select
--->
-<div id="payment-box">
-        <img src="paypal/images/camera.jpg" />
-        <h4 class="txt-title">One Time Payment</h4>
-        <div class="txt-price">$50.00</div>
+#payment-box {
+    padding: 40px;
+    margin: 20px;
+    border: #E4E4E4 1px solid;
+    display: inline-block;
+    text-align: center;
+    border-radius: 3px;
+}
+
+#pay_now {
+    padding: 10px 30px;
+    background: #09f;
+    border: #038fec 1px solid;
+    border-radius: 3px;
+    color: #FFF;
+    width: 100%;
+    cursor: pointer;
+}
+
+.txt-title {
+    margin: 25px 0px 0px 0px;
+    color: #4e4e4e;
+}
+
+.txt-price {
+    margin-bottom: 20px;
+    color: #08926c;
+    font-size: 1.1em;
+}
+</style>
+</head>
+<body>
+    <div id="payment-box">
+        <img src="images/camera.jpg" />
+        <h4 class="txt-title">A6900 MirrorLess Camera</h4>
+        <div class="txt-price">$289.61</div>
         <form action="https://www.sandbox.paypal.com/cgi-bin/webscr"
             method="post" target="_top">
             <input type='hidden' name='business'
                 value='sb-dwdy31641136@business.example.com'> <input type='hidden'
                 name='item_name' value='Camera'> <input type='hidden'
                 name='item_number' value='CAM#N1'> <input type='hidden'
-                name='amount' value='50'> <input type='hidden'
+                name='amount' value='10'> <input type='hidden'
                 name='no_shipping' value='1'> <input type='hidden'
                 name='currency_code' value='USD'> <input type='hidden'
                 name='notify_url'
@@ -31,6 +64,5 @@
                 Value="Pay Now">
         </form>
     </div>
-<?php
-    include('footer.php');
-?>
+</body>
+</html>
--- tables.sql
+++ tables.sql
@@ -59,3 +59,16 @@ CREATE TABLE `member_blog` (
   `body` text NOT NULL,
   PRIMARY KEY (`blogID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+-- Payments table schema
+CREATE TABLE IF NOT EXISTS `payment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_number` varchar(255) NOT NULL,
+  `item_name` varchar(255) NOT NULL,
+  `payment_status` varchar(255) NOT NULL,
+  `payment_amount` double(10,2) NOT NULL,
+  `payment_currency` varchar(255) NOT NULL,
+  `txn_id` varchar(255) NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
