5445a3340a2976992dda15763669bbf0acffd720
calebTree <ande4905@gmail.com>
2025-05-28 12:15:29 -0500

    debug ipn

--- public_html/membership/paypal/notify.php
+++ public_html/membership/paypal/notify.php
@@ -1,100 +1,48 @@
-<?php
-// CONFIG: Enable debug mode. This means we'll log requests into 'ipn.log' in the same directory.
-// Especially useful if you encounter network errors or other intermittent problems with IPN (validation).
-// Set this to 0 once you go live or don't require logging.
-define("DEBUG", 1);
-// Set to 0 once you're ready to go live
-define("USE_SANDBOX", 1);
-define("LOG_FILE", "ipn.log");
-// Read POST data
-// reading posted data directly from $_POST causes serialization
-// issues with array data in POST. Reading raw POST data from input stream instead.
-$raw_post_data = file_get_contents('php://input');
-$raw_post_array = explode('&', $raw_post_data);
-$myPost = array();
-foreach ($raw_post_array as $keyval) {
-	$keyval = explode('=', $keyval);
-	if (count($keyval) == 2) {
-		// Since we do not want the plus in the datetime string to be encoded to a space, we manually encode it.
-		if ($keyval[0] === 'payment_date') {
-			if (substr_count($keyval[1], '+') === 1) {
-				$keyval[1] = str_replace('+', '%2B', $keyval[1]);
-			}
-		}
-		$myPost[$keyval[0]] = urldecode($keyval[1]);
-	}
-}
-// read the post from PayPal system and add 'cmd'
-$req = 'cmd=_notify-validate';
-if(function_exists('get_magic_quotes_gpc')) {
-	$get_magic_quotes_exists = true;
-}
-foreach ($myPost as $key => $value) {
-	if($get_magic_quotes_exists == true && get_magic_quotes_gpc() == 1) {
-		$value = urlencode(stripslashes($value));
-	} else {
-		$value = urlencode($value);
-	}
-	$req .= "&$key=$value";
-}
-// Post IPN data back to PayPal to validate the IPN data is genuine
-// Without this step anyone can fake IPN data
-if(USE_SANDBOX == true) {
-	$paypal_url = "https://ipnpb.sandbox.paypal.com/cgi-bin/webscr";
-} else {
-	$paypal_url = "https://ipnpb.paypal.com/cgi-bin/webscr";
-}
-$ch = curl_init($paypal_url);
-if ($ch == FALSE) {
-	return FALSE;
-}
-curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
-curl_setopt($ch, CURLOPT_SSLVERSION, 6);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
-// if(DEBUG == true) {
-// 	curl_setopt($ch, CURLOPT_HEADER, 1);
-// 	curl_setopt($ch, CURLINFO_HEADER_OUT, 1);
-// }
-// CONFIG: Optional proxy configuration
-//curl_setopt($ch, CURLOPT_PROXY, $proxy);
-//curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, 1);
-// Set TCP timeout to 30 seconds
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-	'User-Agent: PHP-IPN-Verification-Script',
-	'Connection: Close',
-));
+<?php namespace Listener;
+include("DBController.php");
+require('PaypalIPN.php');
+
+use DBController;
+use PaypalIPN;
+use Exception;
+
+$ipn = new PaypalIPN();
+
+// Use the sandbox endpoint during testing.
+$ipn->useSandbox();
+$verified = $ipn->verifyIPN();
+
 // CONFIG: Please download 'cacert.pem' from "http://curl.haxx.se/docs/caextract.html" and set the directory path
 // of the certificate as shown below. Ensure the file is readable by the webserver.
 // This is mandatory for some environments.
 //$cert = __DIR__ . "./cacert.pem";
 //curl_setopt($ch, CURLOPT_CAINFO, $cert);
-$res = curl_exec($ch);
-if (curl_errno($ch) != 0) // cURL error
-{
-	if(DEBUG == true) {	
-		error_log(date('[Y-m-d H:i e] '). "Can't connect to PayPal to validate IPN message: " . curl_error($ch) . PHP_EOL, 3, LOG_FILE);
+// $res = curl_exec($ch);
+// if (curl_errno($ch) != 0) // cURL error
+// {
+// 	if(DEBUG == true) {	
+// 		error_log(date('[Y-m-d H:i e] '). "Can't connect to PayPal to validate IPN message: " . curl_error($ch) . PHP_EOL, 3, LOG_FILE);
+// 	}
+// 	curl_close($ch);
+// 	exit;
+// } else {
+// 		// Log the entire HTTP response if debug is switched on.
+// 		if(DEBUG == true) {
+// 			error_log(date('[Y-m-d H:i e] '). "HTTP request of validation request:". curl_getinfo($ch, CURLINFO_HEADER_OUT) ." for IPN payload: $req" . PHP_EOL, 3, LOG_FILE);
+// 			error_log(date('[Y-m-d H:i e] '). "HTTP response of validation request: $res" . PHP_EOL, 3, LOG_FILE);
+// 		}
+// 		curl_close($ch);
+// }
+
+if ($verified) {
+	if ( ! count($_POST)) {
+		throw new Exception("Missing POST Data");
 	}
-	curl_close($ch);
-	exit;
-} else {
-		// Log the entire HTTP response if debug is switched on.
-		if(DEBUG == true) {
-			error_log(date('[Y-m-d H:i e] '). "HTTP request of validation request:". curl_getinfo($ch, CURLINFO_HEADER_OUT) ." for IPN payload: $req" . PHP_EOL, 3, LOG_FILE);
-			error_log(date('[Y-m-d H:i e] '). "HTTP response of validation request: $res" . PHP_EOL, 3, LOG_FILE);
-		}
-		curl_close($ch);
-}
-// Inspect IPN validation result and act accordingly
-// Split response headers and payload, a better way for strcmp
-$tokens = explode("\r\n\r\n", trim($res));
-$res = trim(end($tokens));
-if (strcmp ($res, "VERIFIED") == 0) {
+	/*
+     * Process IPN
+     * A list of variables is available here:
+     * https://developer.paypal.com/webapps/developer/docs/classic/ipn/integration-guide/IPNandPDTVariables/
+     */
 	// assign posted variables to local variables
 	$item_name = $_POST['item_name'];
 	$item_number = $_POST['item_number'];
@@ -105,7 +53,6 @@ if (strcmp ($res, "VERIFIED") == 0) {
 	$receiver_email = $_POST['receiver_email'];
 	$payer_email = $_POST['payer_email'];
 	
-	include("DBController.php");
 	$db = new DBController();
 	
 	// check whether the payment_status is Completed
@@ -117,7 +64,7 @@ if (strcmp ($res, "VERIFIED") == 0) {
 	$isUniqueTxnId = false; 
 	$param_type="s";
 	$param_value_array = array($txn_id);
-	$result = $db->runQuery("SELECT * FROM payment WHERE txn_id = ?",$param_type,$param_value_array);
+	$result = $db->runQuery("SELECT * FROM payment WHERE txn_id = ?", $param_type,$param_value_array);
 	if(empty($result)) {
         $isUniqueTxnId = true;
 	}	
@@ -127,20 +74,22 @@ if (strcmp ($res, "VERIFIED") == 0) {
 	    $param_type = "sssdss";
 	    $param_value_array = array($item_number, $item_name, $payment_status, $payment_amount, $payment_currency, $txn_id);
 	    $payment_id = $db->insert("INSERT INTO payment(item_number, item_name, payment_status, payment_amount, payment_currency, txn_id) VALUES(?, ?, ?, ?, ?, ?)", $param_type, $param_value_array);
-	    error_log(date('[Y-m-d H:i e] '). "Vdddddddddddderified IPN: $req ". PHP_EOL, 3, LOG_FILE);
+	    // error_log(date('[Y-m-d H:i e] '). "Vdddddddddddderified IPN: $req ". PHP_EOL, 3, LOG_FILE);
 	} 
 	// process payment and mark item as paid.
 	
-	if(DEBUG == true) {
-		error_log(date('[Y-m-d H:i e] '). "Verified IPN: $req ". PHP_EOL, 3, LOG_FILE);
-	}
+	// if(DEBUG == true) {
+	// 	error_log(date('[Y-m-d H:i e] '). "Verified IPN: $req ". PHP_EOL, 3, LOG_FILE);
+	// }
 	
-} else if (strcmp ($res, "INVALID") == 0) {
-	// log for manual investigation
-	// Add business logic here which deals with invalid IPN messages
-	if(DEBUG == true) {
-		error_log(date('[Y-m-d H:i e] '). "Invalid IPN: $req" . PHP_EOL, 3, LOG_FILE);
-	}
+	// } else if (strcmp ($res, "INVALID") == 0) {
+	// 	// log for manual investigation
+	// 	// Add business logic here which deals with invalid IPN messages
+	// if(DEBUG == true) {
+	// 	error_log(date('[Y-m-d H:i e] '). "Invalid IPN: $req" . PHP_EOL, 3, LOG_FILE);
+	// }
 }
+
+// Reply with an empty 200 response to indicate to paypal the IPN was received correctly.
 header("HTTP/1.1 200 OK");
 ?>
