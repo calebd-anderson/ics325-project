1844029679b46ccb41170b7aa16ebbee0f3f91f3
calebTree <ande4905@gmail.com>
2025-05-28 21:59:23 -0500

    remove odd web name

--- public_html/membership/paypal/header.php
+++ public_html/membership/paypal/header.php
@@ -6,7 +6,7 @@
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
   <head>
       <meta charset="utf-8">
-      <title>Chasing Antartica</title>
+      <title>Donation</title>
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <link rel="page icon" href="../favicon.ico" />
       <!-- Homebrew Style -->
